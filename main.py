45317d7fddf6040cdd551f00fd59248ea63a1606
jorizpre <jorizpre@gmail.com>
2023-02-07 21:22:49 +0100

    Created Camera Screen

---
+++ main.py
@@ -0,0 +1,38 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import time
+
+Builder.load_file('frontend.kv')
+
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play=True
+        self.ids.camera.texture = self.ids.camera._camera.texture
+
+    def stop(self):
+        self.ids.camera.play=False
+        self.ids.camera.texture = None
+
+    def capture(self):
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        filename = f"files/{current_time}.png"
+        self.ids.camera.export_to_png(filename)
+
+
+
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
+    pass
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+
+# Running the App
+MainApp().run()
